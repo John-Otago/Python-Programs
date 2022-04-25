789b9c17725741807691137c35d5495b610d737b
John-Otago <82292576+John-Otago@users.noreply.github.com>
2022-04-25 19:57:27 +1200

    Create print_sorbet_flavors.py

---
+++ Beginner Exercises/print_sorbet_flavors.py
@@ -0,0 +1,52 @@
+########################################################################################
+# Exercise at HackInScience:
+# https://www.hackinscience.org/exercises/print-sorbet-flavors
+
+# Print sorbet flavors
+# Created by Julien Palard
+# 
+# You're working for a restaurant and they're asking you to generate the sorbet menu.
+# Provide a script printing every possible sorbet duos from a given list of flavors.
+# Don't print recipes with twice the same flavor (no "Chocolate Chocolate"), and don't 
+# print twice the same recipe (if you print "Vanilla Chocolate", don't print "Chocolate
+# Vanilla", or vice-versa).
+# 
+# The flavors are:
+# 
+# FLAVORS = [
+#     "Banana",
+#     "Chocolate",
+#     "Lemon",
+#     "Pistachio",
+#     "Raspberry",
+#     "Strawberry",
+#     "Vanilla",
+# ]
+# 
+# Print one duo per line, and separate flavors by comas, so your output should look like:
+# 
+# Banana, Chocolate
+# Banana, Lemon
+# Banana, Pistachio
+# Banana, Raspberry
+# Banana, Strawberry
+# Banana, Vanilla
+# Chocolate, Lemon
+# …
+
+########################################################################################
+
+# Solution 1 - the easiest method
+
+for i in FLAVORS:
+    for j in range(FLAVORS.index(i)+1, len(FLAVORS)):
+        print(f"{i}, {FLAVORS[j]}")
+
+########################################################################################
+
+# Solution 2 - to be added later
+
+
+
+
+########################################################################################
