94b8a173c8baa89a41f91a57a471cbb4db228c36
John-Otago <82292576+John-Otago@users.noreply.github.com>
2022-04-29 15:27:55 +1200

    Update rock_paper_scissors_lizard_spock.py

--- My Own Exercises/rock_paper_scissors_lizard_spock.py
+++ My Own Exercises/rock_paper_scissors_lizard_spock.py
@@ -1,3 +1,5 @@
+###############################################################################################################################
+
 # Rock, Paper, Scissors, Lizard, Spock
 
 # This is a challenge that I set for Python beginners:
@@ -6,6 +8,21 @@
 # The game is a twist on the classic Rock, Paper, Scissors, as explained by Dr. Sheldon Lee Cooper in The Big Bang Theory:
 # https://youtu.be/_PUEoDYpUyQ
 # It looks like the game was created by Sam Kass with Karen Bryla, and ‘Spock’ probably comes from the TV franchise Star Trek. 
+
+# It goes like this:
+
+#     Scissors cuts Paper
+#     Paper covers Rock
+#     Rock crushes Lizard
+#     Lizard poisons Spock
+#     Spock smashes Scissors
+#     Scissors decapitates Lizard
+#     Lizard eats Paper
+#     Paper disproves Spock
+#     Spock vaporizes Rock
+#     (and as it always has) Rock crushes Scissors
+
+###############################################################################################################################
 
 # Hint:
 # user_input = int(input("Please enter 1 for Rock, 2 for Paper, 3 for Scissors, 4 for Lizard, and 5 for Spock: "))
