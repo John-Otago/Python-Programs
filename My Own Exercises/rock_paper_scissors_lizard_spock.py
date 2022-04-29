d4d3d971699dbcf8d6b06c4df1ff3305f6228f92
John-Otago <82292576+John-Otago@users.noreply.github.com>
2022-04-29 15:25:32 +1200

    Create rock_paper_scissors_lizard_spock.py

---
+++ My Own Exercises/rock_paper_scissors_lizard_spock.py
@@ -0,0 +1,11 @@
+# Rock, Paper, Scissors, Lizard, Spock
+
+# This is a challenge that I set for Python beginners:
+# Using Python to create a single-player game (user vs. the computer) of Rock, Paper, Scissors, Lizard, Spock.
+
+# The game is a twist on the classic Rock, Paper, Scissors, as explained by Dr. Sheldon Lee Cooper in The Big Bang Theory:
+# https://youtu.be/_PUEoDYpUyQ
+# It looks like the game was created by Sam Kass with Karen Bryla, and ‘Spock’ probably comes from the TV franchise Star Trek. 
+
+# Hint:
+# user_input = int(input("Please enter 1 for Rock, 2 for Paper, 3 for Scissors, 4 for Lizard, and 5 for Spock: "))
