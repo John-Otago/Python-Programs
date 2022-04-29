342f84400a747a2aadbfc8c93f7b1768d7edb5a2
John-Otago <82292576+John-Otago@users.noreply.github.com>
2022-04-29 15:00:53 +1200

    Update character_input.py

--- Beginner Exercises/character_input.py
+++ Beginner Exercises/character_input.py
@@ -11,9 +11,9 @@
 #
 # Extras:
 #
-# Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. 
-# (Hint: order of operations exists in Python)
-# Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button).
+# 1. Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. 
+#    (Hint: order of operations exists in Python)
+# 2. Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button).
 
 ##########################################################################################################################################
 
