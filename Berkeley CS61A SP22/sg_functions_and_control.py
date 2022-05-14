e74ca6e8b8c48e7c92f2831e19bd7e62bdd62aa1
John-Otago <82292576+John-Otago@users.noreply.github.com>
2022-05-14 15:33:58 +1200

    Update sg_functions_and_control.py

--- Berkeley CS61A SP22/sg_functions_and_control.py
+++ Berkeley CS61A SP22/sg_functions_and_control.py
@@ -234,3 +234,52 @@ def in_hailstone(a, b):
 # Test the function
 run_docstring_examples(same_hailstone, globals(), True)
 ###############################################################################
+
+# Q15: Nearest Power of Two
+#
+# Implement the function nearest_two, which takes as input a positive number x
+# and returns the power of two (..., 1/8, 1/4, 1/2, 1, 2, 4, 8, ...) that is
+# nearest to x. If x is exactly between two powers of two, return the larger.
+#
+# You may change the starter implementation if you wish.
+
+
+def nearest_two(x):
+    """Return the power of two that is nearest to x.
+
+    >>> nearest_two(8)    # 2 * 2 * 2 is 8
+    8.0
+    >>> nearest_two(11.5) # 11.5 is closer to 8 than 16
+    8.0
+    >>> nearest_two(14)   # 14 is closer to 16 than 8
+    16.0
+    >>> nearest_two(2015)
+    2048.0
+    >>> nearest_two(.1)
+    0.125
+    >>> nearest_two(0.75) # Tie between 1/2 and 1
+    1.0
+    >>> nearest_two(1.5)  # Tie between 1 and 2
+    2.0
+
+    """
+
+    # Official solution:
+    power_of_two = 1.0
+    if x < 1:
+        factor = 0.5
+    else:
+        factor = 2
+    while abs(power_of_two * factor - x) < abs(power_of_two - x):
+        power_of_two = power_of_two * factor
+    if abs(power_of_two * 2 - x) == abs(power_of_two - x):
+        power_of_two = power_of_two * 2
+    return power_of_two
+
+    # This implementation repeatedly doubles or halves the number power_of_two
+    # until reaching the closest number to x. The last three lines enforce the
+    # tie-breaking policy when x is exactly [between] two powers of two.
+
+
+run_docstring_examples(nearest_two, globals(), True)
+###############################################################################
