07893e63aa5ab5f2af6f33b28f1a7b471c7de5ab
John-Otago <82292576+John-Otago@users.noreply.github.com>
2022-05-23 12:50:10 +1200

    Create fizzbuzz.py

---
+++ Beginner Exercises/fizzbuzz.py
@@ -0,0 +1,12 @@
+# Fizz buzz (from 1 to 20)
+# https://en.wikipedia.org/wiki/Fizz_buzz
+
+for i in range(1, 21):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
